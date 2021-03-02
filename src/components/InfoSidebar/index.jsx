2285b08992f2889eecc2335e63f7e293defc9e78
nostam <matsonho@gmail.com>
2021-03-02 18:45:27 +0100

    add infosidebar draft component

---
+++ src/components/InfoSidebar/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { RiPencilFill } from "react-icons/ri";
+import "styles.scss";
+
+export default function InfoSidebar() {
+  return (
+    <div id="infosidebar">
+      <div className="header"></div>
+      <div className="avatar"></div>
+      <div className="description"></div>
+      <div className="media"></div>
+      <div className="mute-notifications"></div>
+      <div className="star-messages"></div>
+      <div className="participants"></div>
+      <div className="exit"></div>
+      <div className="exit"></div>
+    </div>
+  );
+}
