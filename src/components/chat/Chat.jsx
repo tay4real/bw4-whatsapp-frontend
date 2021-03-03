3c69da13f27675650c016ff2a89f264e8daaae4b
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-03-03 18:02:38 +0500

    Chat component fineshed

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.style.scss";
+import { RiSendPlane2Line } from "react-icons/ri";
+import Picker from "emoji-picker-react";
+import { GrEmoji } from "react-icons/gr";
+import { MdAttachFile } from "react-icons/md";
+import { BsFillMicFill } from "react-icons/bs";
+
+const EmojiPicker = ({ show }) => {
+  return show ? (
+    <div id="emoji-picker-react">
+      <Picker />
+    </div>
+  ) : (
+    <></>
+  );
+};
 
 export default function Chat() {
-  return <div id="chat-component">CHAT</div>;
+  const [message, setMessage] = useState("");
+
+  const [showEmoji, setEmojiShow] = useState(false);
+  const toggleshowEmoji = () => setEmojiShow(!showEmoji);
+
+  return (
+    <div id="chat-component">
+      <EmojiPicker show={showEmoji} />
+      <div id="message-wrapper">
+        <GrEmoji size={30} onClick={toggleshowEmoji} />
+
+        <label>
+          <MdAttachFile size={25} style={{ margin: "0 10px" }} />
+
+          <input accept="image/*" id="icon-button-file" type="file" />
+        </label>
+
+        <input
+          type="text"
+          name=""
+          id="input-message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type a message"
+        />
+        <BsFillMicFill size={25} />
+      </div>
+    </div>
+  );
 }
