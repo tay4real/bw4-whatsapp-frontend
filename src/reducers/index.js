0e01deeb235d70852a93b9fae4e4c9f0437f8f3d
nostam <matsonho@gmail.com>
2021-03-03 13:33:21 +0100

    update rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
 import user from "./userReducer";
-
+import componentsReducer from "./compoenetsReducer";
 const rootReducer = combineReducers({
   user,
+  components: componentsReducer,
 });
 
 export default rootReducer;
