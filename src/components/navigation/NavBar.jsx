00c81ffdc0e26f1059cf0382ea35dd851508236e
nostam <matsonho@gmail.com>
2021-03-05 09:45:44 +0100

    add conditional info in navbar

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -6,32 +6,36 @@ import { BsSearch } from "react-icons/bs";
 import { ProfileImg } from "..";
 
 const NavBar = () => {
-  const { updatedAt, roomName, avatar, isGroup, members} = useSelector(
+  const { updatedAt, roomName, avatar, isGroup, members } = useSelector(
     (state) => state.currentChatRoom
   );
   const { userInfos } = useSelector((state) => state.user);
   const { showInfoSidebar } = useSelector((state) => state.components);
-  // console.log(userInfos.username);
 
   return (
     <div id="navigation">
       <div className="mx-3">
-        <ProfileImg
-          avatar={
-            isGroup
-              ? avatar
-              : members?.filter((e) => e._id !== userInfos._id)[0].avatar
-          }
-          style={{ display: to._id ? "inline-block" : "none" }}
-        />
-        <div id="nav-userInfo" className="mr-1">
-          <h6>
-            {isGroup
-              ? roomName
-              : members?.filter((e) => e._id !== userInfos._id)[0].firtName}
-          </h6>
-          <small>{updatedAt}</small>
-        </div>
+        {members.length !== 0 && (
+          <>
+            <ProfileImg
+              avatar={
+                isGroup
+                  ? avatar
+                  : members?.filter((e) => e._id !== userInfos._id)[0].avatar
+              }
+              style={{ display: roomName ? "inline-block" : "none" }}
+            />
+            <div id="nav-userInfo" className="mr-1">
+              <h6>
+                {isGroup
+                  ? roomName
+                  : members?.filter((e) => e._id !== userInfos._id)[0]
+                      .firstName}
+              </h6>
+              <small>{updatedAt}</small>
+            </div>
+          </>
+        )}
       </div>
 
       <div className={showInfoSidebar === true ? "three" : "two"}>
