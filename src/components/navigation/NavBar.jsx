9320aeec1730ad4cdd8cc89e9ffbdca4831d8759
nostam <matsonho@gmail.com>
2021-03-05 02:23:30 +0100

    update conditional navbar

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -7,15 +7,27 @@ import { ProfileImg } from "..";
 
 const NavBar = () => {
   const { to } = useSelector((state) => state.currentChat);
+  const { userInfos } = useSelector((state) => state.user);
   const { showInfoSidebar } = useSelector((state) => state.components);
   // console.log(userInfos.username);
 
   return (
     <div id="navigation">
       <div className="mx-3">
-        <ProfileImg avatar={to.avatar} />
-        <div id="nav-userInfo" className="mr-">
-          <h6>{to.firstName}</h6>
+        <ProfileImg
+          avatar={
+            to && to.isGroup
+              ? to.avatar
+              : to.members?.filter((e) => e._id !== userInfos._id)[0].avatar
+          }
+          style={{ display: to._id ? "inline-block" : "none" }}
+        />
+        <div id="nav-userInfo" className="mr-1">
+          <h6>
+            {to.isGroup
+              ? to.roomName
+              : to.members?.filter((e) => e._id !== userInfos._id)[0].firtName}
+          </h6>
           <small>{to.updatedAt}</small>
         </div>
       </div>
